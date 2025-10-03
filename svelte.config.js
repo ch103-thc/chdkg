01e71500372acd22bd179580980bad58d8df8d5a
Chloe <teochloe213@gmail.com>
2025-10-03 17:33:19 +0800

    corrected vercel adapter mistake

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
-import sveltePreprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: sveltePreprocess(),
   kit: {
     adapter: adapter()
   }
